87cad17e8520aac52e49335d0564ff867c32dda1
Unlimited Coders <unlimitedcoders@Unlimiteds-Mac-mini.local>
2024-09-23 17:38:25 +0200

    Fourht commit

--- index.js
+++ index.js
@@ -19,17 +19,13 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Shinnig",url:"https://covers.openlibrary.org/b/olid/OL53210900M-M.jpg" },
-  { id: 2, title: "Waking up" ,url:"https://covers.openlibrary.org/b/olid/OL32164562M-M.jpg"},
-  { id: 3, title: "Finish homework",url:"https://covers.openlibrary.org/b/olid/OL32047814M-M.jpg" },
-  { id: 4, title: "Finish homework",url:"" }
-];
 
 app.get("/", async (req, res) => {
-  let urls = items.map(item => item.url);
-  urls = urls.filter(url => url);
-  console.log(urls)
+  //Image url From base
+  let text
+  const result = await db.query("SELECT image_url FROM book ORDER BY id ASC");
+  const urlPg = result.rows
+  let urls = urlPg.map(item => item.image_url);
   
   try {
     const responses = await Promise.all(
@@ -45,6 +41,45 @@ app.get("/", async (req, res) => {
     
     // You can now use these base64 strings in your app
     images.map(image => `data:image/jpeg;base64,${image}`)
+    
+    //Wokrs url from api
+    const works = await db.query("SELECT work_url FROM book ORDER BY id ASC");
+    const urlWork = works.rows
+    let urlsWork = urlWork.map(item => item.work_url);
+    console.log(urlsWork)
+    
+    try{
+      const responses = await Promise.all(
+        urlsWork.map(url => axios.get(url)
+      
+      )
+    )
+    const workUrls = responses.map(response => {
+      const description = response.data.description;
+      
+      if (typeof description === 'object') {
+        return description.value
+      }
+      else {
+        return description
+      }
+    
+      })
+      text = workUrls
+      text = text.map(text => text.replace(/[\r\n]+/g, ' ') // Replace \r\n with a space
+      .replace(/\+/g, '') // Remove all '+' signs
+      .replace(/,/g, '') // Remove all ',' signs
+      .replace(/\//g, '') // Remove all '/' signs
+      .replace(/----------/g, '') // Remove all occurrences of '----------'
+      .replace(/-\s*\[/g, '') // Remove occurrences of '- [' (including space after '-')
+      .trim())
+      console.log(text)
+    
+    
+    }catch(error){
+      console.log('error fetching work details')
+    }
+ 
   
     res.render('index.ejs',{listItems:images})
     
@@ -52,6 +87,10 @@ app.get("/", async (req, res) => {
     console.error('Error fetching images:', error);
   }
   
+
+
+ 
+
 });
 
 
